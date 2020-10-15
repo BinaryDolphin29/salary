eb09c90dcf829ca545997913b9b283ffd00ca1dd
yuuyuu1396 <m4160717428@yahoo.co.jp>
2020-10-15 20:28:13 +0900

    change fie

---
+++ calc.js
@@ -0,0 +1,93 @@
+const base = 964                    // 基本時給 (9:00 ~ 18:00)
+// const A = (930 + (base - 910)) / 4  // 9:00 以前
+// const B = (1010 + (base - 910)) / 4 // 18:00 ~ 19:00
+// const C = (1040 + (base - 910)) / 4 // 19:00 ~ 20:00
+// const D = (1080 + (base - 910)) / 4 // 20:00 以降
+
+module.exports = class {
+  /** @example
+   * new Salary([
+   *  {
+   *    start: 12,
+   *    end: 20.5,
+   *    count: 4
+   *  }
+   * ]) */
+  constructor(data) {
+    if (!Array.isArray(data) && !data.filter(v => v instanceof Object).length >= 1) {
+      throw new TypeError("引数が無効です。")
+    }
+
+    this.base = 971                    // 基本時給 (9:00 ~ 18:00)
+    this.A = (930 + (base - 910)) / 4  // 9:00 以前
+    this.B = (1010 + (base - 910)) / 4 // 18:00 ~ 19:00
+    this.C = (1040 + (base - 910)) / 4 // 19:00 ~ 20:00
+    this.D = (1080 + (base - 910)) / 4 // 20:00 以降
+
+    this.data = data
+    this._res = 0
+    this.hours = 0
+    this.days = 0
+  }
+
+  /** 
+   * コンストラクタの引数をもとに支給額合計を計算します。
+   * @returns {Number}
+  */
+  Calc() {
+    for (const day of this.data) {
+      const { start, end, count } = day
+      this.days += count
+
+      for (var n = 0; n < count; n++) {
+        this.hours += end - start
+
+        for (var i = start; i < end; i += 0.25) {
+          if (i <= 17.75) this._res += this.base / 4
+          if (i >= 18 && i <= 18.75) this._res += this.B
+          if (i >= 19 && i <= 19.75) this._res += this.C
+          if (i >= 20) this._res += this.D
+        }
+      }
+    }
+
+    return this._res
+  }
+
+  /**
+   * 基礎時給を計算します。
+   * @returns {Number}
+  */
+  get Kisojikyuu() {
+    return base*this.hours
+  }
+  
+  /**
+   * 調整給を計算します。
+   * @returns {Number}
+  */
+  get Tyouseikyuu() {
+    return this._res - this.Kisojikyuu
+  }
+
+  /**
+   * 通勤手当を計算します。
+   * @returns {Number}
+  */
+  get Tuukinteate() {
+    if (this.days > 16) return 1000
+    return Math.round((1000 / 16.99) * this.days)
+  }
+
+  /**
+   * <Number> を日本円へフォーマットします。
+   * @param {Number} n
+   * @returns {String}
+   */
+  static ToJAPANESE_YEN(n = 0) {
+    return new Intl.NumberFormat("ja-JP", {
+      style: "currency",
+      currency: "JPY"
+    }).format(n)
+  }
+} 
---
+++ readme.md
@@ -0,0 +1,25 @@
+自分専用のバイトの給与計算ライブラリ？です。  
+
+# Exsample
+```javascript
+const Calc = require("./calc")
+const salary = new Calc([
+    {
+        start: 12.5,
+        end: 20.5,
+        count: 4
+    },
+    {
+        start: 8,
+        end: 14,
+        count: 2
+    }
+])
+
+const res = salary.Calc()
+console.log(Calc.ToJAPANESE_YEN(res))
+```
+
+# 環境
+* [Node.js](https://nodejs.org/ja/) `(v13.6.0)`
+* Windows 10 `(v1909)`
